29b6a44add54cc8a611c051bf65f21f972834bc8
Angie Liceth <liceth5262@gmail.com>
2024-08-14 19:07:53 -0500

    se empezo los estilos del header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,35 @@
 import { NavLink } from "react-router-dom";
+import { Tabs, TabList, Tab, TabIndicator, TabPanels, TabPanel } from "@chakra-ui/react";
+import logo from "../../assets/images/shared/logo.svg"
+import { Flex } from "@chakra-ui/react"
 
 const Header = () => {
-    const links = [
-        { label: "Home", path: "/" },
-        { label: "Destination", path: "/Destination" },
-        { label: "Crew", path: "/Crew" },
-        { label: "Techonology", path: "/Technology" },
-      ];
-  return (
-    <ul>
-        {links.map((link, index) => (
-          <li key={index}>
-            <NavLink
-              className={({ isActive }) =>
-                [isActive ? "link active" : "link"].join(" ")
-              }
-              to={link.path}
-            >
-              {link.label}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-  )
-}
+  const links = [
+    { label: "00 HOME", path: "/" },
+    { label: "01 DESTINATION", path: "/Destination" },
+    { label: "02 CREW", path: "/Crew" },
+    { label: "03 TECHNOLOGY", path: "/Technology" },
+  ];
 
-export default Header
+  return (
+    <>
+      < Flex flexDir='row' gap='30px'>
+        <header >
+          <img src={logo} />
+          <Tabs position='relative' variant='unstyled' bg="black">
+            <TabList>
+              {links.map((link, index) => (
+                <Tab color="white" key={index} as={NavLink} to={link.path} >
+                  {link.label}
+                </Tab>
+              ))}
+            </TabList>
+            <TabIndicator mt='-3px' height='3px' bg='white' borderRadius='1px' />
+          </Tabs>
+        </header>
+      </Flex>
+    </>
+  );
+};
+
+export default Header;
