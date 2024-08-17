954353387dae5a6d2a7238af8751b89e08185907
Angie Liceth <liceth5262@gmail.com>
2024-08-17 17:13:09 -0500

    home terminado en desktop en un 90%

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,14 +5,24 @@ const Home = () => {
     <>
       <div className="home-background min-h-screen bg-cover bg-no-repeat">
         <Header />
-        <div className="flex flex-row px-6 py-16 md:px-24 md:py-32 space-y-8 md:space-y-0">
-          <div className="text-tertiary flex-col ">
-            <h2>SO, YOU WANT TO TRAVEL TO</h2>
-            <h2>SPACE</h2>
-            <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+        <div className="flex flex-row justify-around pt-80 pb-20">
+          <div className="text-tertiary flex-col w-1/3">
+            <h2 className='home-h2 text-3xl pb-2 tracking-widest'>SO, YOU WANT TO TRAVEL TO</h2>
+            <h2 className='space text-[150px] pb-2'>SPACE</h2>
+            <p className='parrafo text-lg'>
+              Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+            </p>
           </div>
-          <div>
-            <button className='bg-tertiary text-primary w-24 h-24 rounded-full mx-auto'>EXPLORE</button>
+          <div className='pt-36 flex justify-center items-center'>
+            <div className='relative'>
+              {/* Botón Explore */}
+              <button className='button-explore bg-tertiary text-primary text-3xl tracking-widest w-64 h-64 rounded-full z-20 relative transition-all duration-300 hover:bg-white'>
+                EXPLORE
+              </button>
+
+              {/* Círculo más grande y opaco al hacer hover */}
+              <div className='absolute top-0 left-0 w-64 h-64 bg-tertiary rounded-full opacity-50 transition-transform duration-300 transform scale-100 hover:scale-150 hover:opacity-100'></div>
+            </div>
           </div>
         </div>
       </div>
