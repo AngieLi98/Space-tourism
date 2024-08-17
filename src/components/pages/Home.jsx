e9ca523faa1d6524578d9bef4d061ad4792d72fb
Angie Liceth <liceth5262@gmail.com>
2024-08-16 19:23:18 -0500

    estilos del home 15%

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,22 +3,18 @@ import Header from '../Header/Header';
 const Home = () => {
   return (
     <>
-      {/* Aplicamos la clase de fondo al body o al contenedor principal */}
-      <div className="home-background min-h-screen">
-        {/* Incluye el Header aquí */}
+      <div className="home-background min-h-screen bg-cover bg-no-repeat">
         <Header />
-        
-        {/* Contenido principal de la página */}
-        {/* <div className="flex flex-col items-center justify-center">
-          <div className="text-tertiary">
-          <h2>SO, YOU WANT TO TRAVEL TO</h2>
-          <h2>SPACE</h2>
-          <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+        <div className="flex flex-row px-6 py-16 md:px-24 md:py-32 space-y-8 md:space-y-0">
+          <div className="text-tertiary flex-col ">
+            <h2>SO, YOU WANT TO TRAVEL TO</h2>
+            <h2>SPACE</h2>
+            <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
           </div>
           <div>
-            <img src="" alt="" />
+            <button className='bg-tertiary text-primary w-24 h-24 rounded-full mx-auto'>EXPLORE</button>
           </div>
-        </div> */}
+        </div>
       </div>
     </>
   );
